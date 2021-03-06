e818d9fc972591baeb5eb774e2acbaa9733cdb14
Klis4 <kleshcenok.ilya@gmail.com>
2021-03-06 22:38:56 +0300

    feat: create min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length > 0) return Math.min(...array);
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) return Math.max(...array);
+  return 0
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    const average = (array.reduce((sum, item) => sum + item, 0)) / array.length;
+    return average;
+  }
   return 0;
 }
